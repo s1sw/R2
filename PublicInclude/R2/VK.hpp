500edd1088fac994106396c888ad25bccb2e21bb
Someone Somewhere <someone167@pm.me>
2022-11-20 23:06:35 +0000

    Initial commit

---
+++ PublicInclude/R2/VK.hpp
@@ -0,0 +1,12 @@
+#pragma once
+// Common includes for VK
+#include "VKBuffer.hpp"
+#include "VKCommandBuffer.hpp"
+#include "VKCore.hpp"
+#include "VKDescriptorSet.hpp"
+#include "VKEnums.hpp"
+#include "VKFrameSeparatedBuffer.hpp"
+#include "VKPipeline.hpp"
+#include "VKRenderPass.hpp"
+#include "VKSampler.hpp"
+#include "VKTexture.hpp"
---
+++ README.md
@@ -0,0 +1,4 @@
+R2 
+===
+
+R2 is a simple Vulkan abstraction that slightly simplifies rendering code.
