371bb8a78440a9fb27926a09b5e8c13ebf60aa27
WNP78 <wnp78g@gmail.com>
2023-03-11 17:23:33 +0000

    Fix CommandBuffer::TextureBarrier on non-sync2

--- Source/VK/VKCommandBuffer.cpp
+++ Source/VK/VKCommandBuffer.cpp
@@ -5,6 +5,7 @@
 #include <R2/VKSyncPrims.hpp>
 #include <R2/VKTexture.hpp>
 #include <R2/VKPipeline.hpp>
+#include <VKSyncLegacyHelpers.hpp>
 #include <RenderPassCache.hpp>
 
 namespace R2::VK
@@ -130,6 +131,7 @@ namespace R2::VK
 
     void CommandBuffer::TextureBarrier(Texture* tex, PipelineStageFlags srcStage, PipelineStageFlags dstStage, AccessFlags srcAccess, AccessFlags dstAccess)
     {
+        #ifdef USE_SYNC_2
         VkImageMemoryBarrier2 imageBarrier { VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2 };
         imageBarrier.image = tex->GetNativeHandle();
         imageBarrier.oldLayout = imageBarrier.newLayout = (VkImageLayout)tex->lastLayout;
@@ -145,6 +147,19 @@ namespace R2::VK
         di.pImageMemoryBarriers = &imageBarrier;
         di.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
         vkCmdPipelineBarrier2(cb, &di);
+        #else
+        VkImageMemoryBarrier imageBarrier = { VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER };
+        imageBarrier.image = tex->GetNativeHandle();
+        imageBarrier.oldLayout = imageBarrier.newLayout = (VkImageLayout)tex->lastLayout;
+        tex->lastAccess = dstAccess;
+        imageBarrier.srcAccessMask = getOldAccessFlags(srcAccess);
+        imageBarrier.dstAccessMask = getOldAccessFlags(dstAccess);
+        imageBarrier.subresourceRange = VkImageSubresourceRange { tex->getAspectFlags(), 0, (uint32_t)tex->GetNumMips(), 0, (uint32_t)tex->GetLayerCount() };
+        imageBarrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        imageBarrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+
+        vkCmdPipelineBarrier(cb, getOldPipelineStageFlags(srcStage), getOldPipelineStageFlags(dstStage), 0, 0, nullptr, 0, nullptr, 1, &imageBarrier);
+        #endif
     }
 
     VkOffset3D convertOffset(Offset3D offset)
