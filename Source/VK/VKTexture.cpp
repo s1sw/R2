4ab22ac265777feb4c9c30771a8a550b524c2a7f
WNP78 <wnp78g@gmail.com>
2023-03-11 17:23:07 +0000

    Split out legacy sync helpers

--- Source/VK/VKTexture.cpp
+++ Source/VK/VKTexture.cpp
@@ -4,6 +4,7 @@
 #include <R2/VKDeletionQueue.hpp>
 #include <R2/VKEnums.hpp>
 #include <R2/VKUtil.hpp>
+#include <VKSyncLegacyHelpers.hpp>
 #include <volk.h>
 #include <vk_mem_alloc.h>
 #include <assert.h>
@@ -344,57 +345,6 @@ namespace R2::VK
     TextureFormat Texture::GetFormat()
     {
         return format;
-    }
-
-    bool hasAccessBit(AccessFlags flags, AccessFlags test)
-    {
-        return ((uint64_t)flags & (uint64_t)test) == (uint64_t)test;
-    }
-
-    bool hasStageBit(PipelineStageFlags flags, PipelineStageFlags test)
-    {
-        return ((uint64_t)flags & (uint64_t)test) == (uint64_t)test;
-    }
-
-    VkAccessFlags getOldAccessFlags(AccessFlags access)
-    {
-        VkAccessFlags flagBits = 0;
-        flagBits = (VkAccessFlagBits)((uint64_t)(access) & 0xFFFFFFFF);
-
-        if (hasAccessBit(access, AccessFlags::ShaderSampledRead)  ||
-            hasAccessBit(access, AccessFlags::ShaderStorageRead))
-        {
-            flagBits |= VK_ACCESS_SHADER_READ_BIT;
-        }
-
-        if (hasAccessBit(access, AccessFlags::ShaderStorageWrite))
-        {
-            flagBits |= VK_ACCESS_SHADER_WRITE_BIT;
-        }
-
-        return flagBits;
-    }
-
-    VkPipelineStageFlags getOldPipelineStageFlags(PipelineStageFlags flags)
-    {
-        VkPipelineStageFlags oldFlags = 0;
-        oldFlags = (VkPipelineStageFlags)((uint64_t)(flags) & 0xFFFFFFFF);
-
-        if (hasStageBit(flags, PipelineStageFlags::Copy) ||
-            hasStageBit(flags, PipelineStageFlags::Resolve) ||
-            hasStageBit(flags, PipelineStageFlags::Blit) ||
-            hasStageBit(flags, PipelineStageFlags::Clear))
-        {
-            oldFlags |= VK_PIPELINE_STAGE_TRANSFER_BIT;
-        }
-
-        if (hasStageBit(flags, PipelineStageFlags::IndexInput) ||
-            hasStageBit(flags, PipelineStageFlags::VertexAttributeInput))
-        {
-            oldFlags |= VK_PIPELINE_STAGE_VERTEX_INPUT_BIT;
-        }
-
-        return oldFlags;
     }
 
     void Texture::Acquire(CommandBuffer cb, ImageLayout layout, AccessFlags access, PipelineStageFlags stage)
